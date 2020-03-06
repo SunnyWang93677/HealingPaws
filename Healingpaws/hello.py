168190ad46d53ca6f593051d97c9b87089c7b6f8
SunnyWang93677 <SunnyWang93677@users.noreply.github.com>
2020-03-06 23:12:15 +0800

    update

--- Healingpaws/hello.py
+++ Healingpaws/hello.py
@@ -1,4 +1,6 @@
 #if __name__ == "__main__":
+#from Healingpaws.healingpawsapp import app
+
 print("hello word")
-from diaryapp import *
+from Healingpaws.healingpawsapp import *
 app.run(host="0.0.0.0",port="1315")
