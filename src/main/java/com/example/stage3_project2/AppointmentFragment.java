84e2ca0636f18b736dc98a70c30364121fac9fe1
WangJi42 <61689511+WangJi42@users.noreply.github.com>
2020-03-15 19:36:43 +0800

    Add files via upload
    first files.1

---
+++ src/main/java/com/example/stage3_project2/AppointmentFragment.java
@@ -0,0 +1,31 @@
+package com.example.stage3_project2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AppointmentFragment extends Fragment {
+
+
+    public AppointmentFragment() {
+
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_appointment, container, false);
+    }
+}
