6cfae9cdec2e54cbb9715046008c105e61beafcf
WangJi42 <yaochen.wang@ucdconnect.ie>
2020-03-28 21:38:33 +0800

    Try login. Connect to the server.

---
+++ Stage3_Project2/app/src/main/java/tools/ServerIP.java
@@ -0,0 +1,9 @@
+package tools;
+
+public class ServerIP {
+
+    public static final HOST = "";
+    public static final String LOGURL="http://"+HOST+"/user";
+    public static final String SIGNURL="http://"+HOST+"/register";
+    public static final String POSTURL = "http://"+HOST+"/postdata";
+}
