3d070c7eb6d28d7ceecf074f9402cace5234947e
Joe Wang <zhouheng.wang@ucdconnect.ie>
2020-03-31 01:41:04 +0800

    Update routes.py

--- Healingpaws/healingpawsapp/routes.py
+++ Healingpaws/healingpawsapp/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, session, send_file, request, jsonify
-#from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from healingpawsapp import app, db
-#from healingpawsapp.forms import LoginForm, SignupForm, ProfileForm, PostForm, AddPhotoForm
+from healingpawsapp.forms import LoginForm
 #from appdir.models import User, Post, Profile
 from healingpawsapp.config import Config
 from healingpawsapp.models import Customer,Employee
