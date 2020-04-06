908519abd468a2286fb5077decec49074e4a479c
WangJi42 <yaochen.wang@ucdconnect.ie>
2020-04-06 10:56:04 +0800

    Testing connection
    Still some error on connection to server

--- Stage3_Project2/app/src/main/java/tools/ServerIP.java
+++ Stage3_Project2/app/src/main/java/tools/ServerIP.java
@@ -2,8 +2,8 @@ package tools;
 
 public class ServerIP {
 
-    public static final HOST = "";
-    public static final String LOGURL="http://"+HOST+"/user";
-    public static final String SIGNURL="http://"+HOST+"/register";
+    public static final String HOST = "healingpaws.top:1315";
+    public static final String LOGURL="http://"+HOST+"/employee_login";
+    public static final String SIGNURL=HOST+"/emp_register";
     public static final String POSTURL = "http://"+HOST+"/postdata";
 }
